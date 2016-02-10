72f8151ba1b8700775b9e244d25c372044cd0a17
Eric DEBIEF <e.debief@hager.fr>
2016-02-10 09:46:32 +0100

    move the DRAM drivers file into the driver subdirectory

--- board/sama5d2_proto888A/board.mk
+++ board/sama5d2_proto888A/board.mk
@@ -1,20 +1,24 @@
 CPPFLAGS += -DCONFIG_SAMA5D2_PROTO888A
 ASFLAGS += -DCONFIG_SAMA5D2_PROTO888A
 
+BOARD_DRIVERS_SRC = $(TOPDIR)/board/$(BOARDNAME)/driver
+BOARD_INC_PATH = $(TOPDIR)/board/$(BOARDNAME)/include
+
+#Add the board's dedicated include directory.
+CPPFLAGS += -I$(BOARD_INC_PATH)
+
+
 #Add MMU support
-COBJS-$(CONFIG_WITH_MMU) += $(TOPDIR)/board/$(BOARDNAME)/driver/cp15.o
-SOBJS-$(CONFIG_WITH_MMU) += $(TOPDIR)/board/$(BOARDNAME)/driver/cp15_asm_gcc.o
-COBJS-$(CONFIG_WITH_MMU) += $(TOPDIR)/board/$(BOARDNAME)/driver/mmu.o
-COBJS-$(CONFIG_WITH_MMU) += $(TOPDIR)/board/$(BOARDNAME)/driver/board_memories.o
+COBJS-$(CONFIG_WITH_MMU) += $(BOARD_DRIVERS_SRC)/cp15.o
+SOBJS-$(CONFIG_WITH_MMU) += $(BOARD_DRIVERS_SRC)/cp15_asm_gcc.o
+COBJS-$(CONFIG_WITH_MMU) += $(BOARD_DRIVERS_SRC)/mmu.o
+COBJS-$(CONFIG_WITH_MMU) += $(BOARD_DRIVERS_SRC)/board_memories.o
 
 ifeq ($(CONFIG_WITH_MMU),y)
-CPPFLAGS += -I$(TOPDIR)/board/$(BOARDNAME)/driver
+CPPFLAGS += -I$(BOARD_DRIVERS_SRC)
 endif
-
-#Add DMA support file
-COBJS-$(CONFIG_DATAFLASH_LOAD_WITH_DMA) += $(TOPDIR)/board/$(BOARDNAME)/dma_hardware_interface.o
 
 
 #Add the memory lowlevel init code according to the actual type : only LP-DDR1 !
-COBJS-$(CONFIG_LPDDR1) += $(TOPDIR)/board/$(BOARDNAME)/lpddr1.o
-COBJS-$(CONFIG_RAM_CHIP_W948D2) += $(TOPDIR)/board/$(BOARDNAME)/wd948d2.o
+COBJS-$(CONFIG_LPDDR1) += $(BOARD_DRIVERS_SRC)/lpddr1.o
+COBJS-$(CONFIG_RAM_CHIP_W948D2) += $(BOARD_DRIVERS_SRC)/wd948d2.o
